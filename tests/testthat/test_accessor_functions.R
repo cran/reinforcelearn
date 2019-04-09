59dce37de2a11efb81f71837c13f366a5d17552d
Markus Dumke <markusdumke@gmail.com>
2019-04-09 10:50:08 +0000

    version 0.2.1

--- tests/testthat/test_accessor_functions.R
+++ tests/testthat/test_accessor_functions.R
@@ -10,18 +10,25 @@ test_that("getStateValues returns row max of action value function", {
   expect_equal(getStateValues(matrix(c(1, 2, 3, 4), ncol = 2)), c(3, 4))
 })
 
-set.seed(1)
-context("getReplayMemory")
-env = makeEnvironment("windy.gridworld")
-memory = makeReplayMemory(size = 2L, batch.size = 2L)
-agent = makeAgent("random", replay.memory = memory)
-interact(env, agent, n.steps = 2L)
 
-test_that("getReplayMemory returns list", {
-  expect_equal(typeof(getReplayMemory(agent)), "list")
-  expect_equal(getReplayMemory(agent), list(list(state = 30, action = 0,
-    reward = -1, next.state = 30), list(state = 30, action = 2, reward = -1,
-      next.state = 20)))
-})
+if (!(R.version$major <= 3 && R.version$minor < 6)) {
 
-context("getEligibilityTraces")
+  set.seed(1)
+  context("getReplayMemory")
+  env = makeEnvironment("windy.gridworld")
+  memory = makeReplayMemory(size = 2L, batch.size = 2L)
+  agent = makeAgent("random", replay.memory = memory)
+  interact(env, agent, n.steps = 2L)
+
+  test_that("getReplayMemory returns list", {
+    expect_equal(typeof(getReplayMemory(agent)), "list")
+    expect_equal(getReplayMemory(agent), list(list(state = 30,
+                                                   action = 0,
+                                                   reward = -1,
+                                                   next.state = 30),
+                                              list(state = 30,
+                                                   action = 2,
+                                                   reward = -1,
+                                                   next.state = 20)))
+  })
+}
